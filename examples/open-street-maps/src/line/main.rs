056140a4ce4446302f1ed110fb1b67d3ce58ee40
c1m50c <58411864+c1m50c@users.noreply.github.com>
2024-09-30 16:14:53 -0500

    Add `line` example to `open-street-maps`

---
+++ examples/open-street-maps/src/line/main.rs
@@ -0,0 +1,31 @@
+use geo::line_string;
+use open_street_maps::tile_fetcher;
+use snapper::SnapperBuilder;
+
+fn main() -> Result<(), anyhow::Error> {
+    let snapper = SnapperBuilder::new()
+        .with_tile_fetcher(tile_fetcher)
+        .with_tile_size(256)
+        .with_zoom(15)
+        .build()?;
+
+    let line = geo::line_string![
+        // Chimney Rock, Nebraska
+        // https://www.openstreetmap.org/search?lat=41.703811459356196&lon=-103.34835922605679
+        (x: 41.703811459356196, y: -103.34835922605679),
+
+        // Chimney Rock Cemetery, Nebraska
+        // https://www.openstreetmap.org/search?lat=41.702909695820175&lon=-103.33250120288363
+        (x: 41.69996628239992, y: -103.34170814251178),
+
+        // Chimney Rock Museum, Nebraska
+        // https://www.openstreetmap.org/search?lat=41.702909695820175&lon=-103.33250120288363
+        (x: 41.702909695820175, y: -103.33250120288363),
+    ];
+
+    snapper
+        .generate_snapshot_from_geometry(line, None)?
+        .save("example.png")?;
+
+    Ok(())
+}
