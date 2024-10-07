3fb31f223125a87bea9bcc2a566ac6006c008e02
c1m50c <58411864+c1m50c@users.noreply.github.com>
2024-10-07 18:54:48 -0500

    Implement `snapr_py::Snapr` methods for using underlying `snapr` crate

--- snapr-py/src/lib.rs
+++ snapr-py/src/lib.rs
@@ -1,6 +1,116 @@
-use pyo3::prelude::*;
+use std::io::Cursor;
+
+use ::snapr::SnaprBuilder;
+use image::{DynamicImage, ImageReader};
+use pyo3::{
+    create_exception,
+    exceptions::PyException,
+    prelude::*,
+    types::{PyByteArray, PyDict, PyFunction, PyList},
+};
+use utilities::{to_py_error, to_snapr_error};
+
+mod utilities;
+
+/// Python-variant of the [`snapr::TileFetcher`](::snapr::TileFetcher) type.
+type PyTileFetcher = fn(i32, i32, u8) -> PyResult<Py<PyByteArray>>;
+
+#[derive(Debug)]
+#[pyclass]
+struct Snapr {
+    tile_fetcher: Py<PyFunction>,
+    tile_size: u32,
+    height: u32,
+    width: u32,
+    zoom: Option<u8>,
+}
+
+#[pymethods]
+impl Snapr {
+    #[new]
+    #[pyo3(signature = (tile_fetcher, tile_size=256, height=600, width=800, zoom=None))]
+    fn new(
+        tile_fetcher: Py<PyFunction>,
+        tile_size: u32,
+        height: u32,
+        width: u32,
+        zoom: Option<u8>,
+    ) -> Self {
+        Self {
+            tile_fetcher,
+            tile_size,
+            height,
+            width,
+            zoom,
+        }
+    }
+
+    fn generate_snapshot_from_geometry(&self, geometry: Py<PyDict>) -> PyResult<Py<PyByteArray>> {
+        todo!("Call `self.get_snapper` and generate snapshot using the given `geometry`")
+    }
+
+    fn generate_snapshot_from_geometries(
+        &self,
+        geometries: Py<PyList>,
+    ) -> PyResult<Py<PyByteArray>> {
+        todo!("Call `self.get_snapper` and generate snapshot using the given `geometries`")
+    }
+}
+
+impl Snapr {
+    fn get_snapper(&self) -> PyResult<::snapr::Snapr> {
+        let tile_fetcher = |x, y, zoom| -> Result<DynamicImage, ::snapr::Error> {
+            let image_bytes = Python::with_gil(|py| -> PyResult<Vec<u8>> {
+                let bytes: Py<PyByteArray> = self
+                    .tile_fetcher
+                    .call1(py, (x, y, zoom))
+                    .and_then(|any| any.extract(py))?;
+
+                bytes.extract(py)
+            });
+
+            let cursor = match image_bytes {
+                Ok(bytes) => Cursor::new(bytes),
+
+                Err(err) => {
+                    return Err(to_snapr_error(err));
+                }
+            };
+
+            let image = ImageReader::new(cursor)
+                .with_guessed_format()
+                .map_err(to_snapr_error)?
+                .decode()
+                .map_err(to_snapr_error)?;
+
+            Ok(image)
+        };
+
+        let builder = SnaprBuilder::new()
+            .with_tile_fetcher(tile_fetcher)
+            .with_tile_size(self.tile_size)
+            .with_height(self.height)
+            .with_width(self.width);
+
+        let snapr = match self.zoom {
+            Some(zoom) => {
+                let builder = builder.with_zoom(zoom);
+                builder.build()
+            }
+
+            None => builder.build(),
+        };
+
+        snapr.map_err(to_py_error)
+    }
+}
+
+create_exception!(snapr, SnaprError, PyException);
 
 #[pymodule]
-fn snapr(_: &Bound<'_, PyModule>) -> PyResult<()> {
+fn snapr(py: Python, module: &Bound<'_, PyModule>) -> PyResult<()> {
+    module.add("SnaprError", py.get_type_bound::<SnaprError>())?;
+    module.add_class::<Snapr>()?;
+
     Ok(())
 }
